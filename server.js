82c63a53819d98aea0aca9459c75fd7e3879a058
Amir <146404764+AmirShrymnsh@users.noreply.github.com>
2024-05-20 11:58:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/**
+*  WEB322 - Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites, friends gpt or otherwise) or distributed to other students.
+*  I understand that if caught doing so, I will receive zero on this assignment and possibly 
+*  fail the entire course.
+*  Name: Amirali Shahriarymanesh
+*  Student ID: 153106232
+*  Date:05/17/2024
+*  Vercel Web App URL: https://vercel.com/amirali-shahriarymaneshs-projects/web322
+*  GitHub Repository URL: https://github.com/AmirShrymnsh/web322
+**/
+
 console.log("Full Name : Amirali Shahriarymanesh"); 
 console.log("Student ID: 153106232"); 
 
